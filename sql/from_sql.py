12fd0d70ad180823e358eb04a7ef3195b6b0e949
Dan Murphy <danielmurph8@gmail.com>
2020-07-08 18:36:41 -0400

    Normalized Data

--- sql/from_sql.py
+++ sql/from_sql.py
@@ -50,9 +50,26 @@ def clean_data(data, columns_to_drop):
     """
     data = data.drop(columns_to_drop, axis = 1)
     return data
+
 clean_df = clean_data(raw_df, ['i', 'event_name'])
+
+def normalize_data(data, column_names):
+    """
+    normalizes a specified column in clean_df. Technique used is (x - x_min)/(x_max - x_min)
+    @params : data, a dataset and column_names, the columns to normalize. The column must be in the 
+              dataset
+    @returns : the normalized dataset
+    """
+    for i in range(len(column_names)):
+        x_max = max(data[column_names[i]])
+        x_min = min(data[column_names[i]])
+        data[column_names[i]] = data[column_names[i]].apply(
+            lambda val : (val - x_min)/(x_max - x_min) 
+        )
+    return data
+
+normed_df = normalize_data(clean_df, ['photoresistor', 'temp', 'humidity'])
 
 
 if __name__ == '__main__':
-    print(clean_df)
     connection.close()
